2411a982d26a93f0f105f7f46d07a7da9b2c774b
maximewel <maxime.welcklen@he-arc.ch>
2020-12-21 11:01:58 +0100

    refactor local unit test into a cleaner version using sed

--- scripts-ci/local-unit-test.sh
+++ scripts-ci/local-unit-test.sh
@@ -23,32 +23,31 @@ echo "generating..."; fprime-util generate
 
 echo "Searchign Unit Test on $base_directory..."
 #get directories that have test in them
-dirs=($(find $base_directory -type d -name "test" -not -path "$base_directory/build-*"))
+dirs=($(find $base_directory -type d -name "ut" -not -path "$base_directory/build-*" | sed -e 's/\<test\/ut\>//g'))
 #number of dirs
 processed_tests=${#dirs[@]}
 
+echo -e "\n\n\nfound directories : "
 for dir in "${dirs[@]}"
 do
-	echo "found dirs : $dir"
+	echo -e "$dir"
 done
+echo -e "\n\n\n"
 
 #iterate over directories to build their UT
 for dir in "${dirs[@]}"
 do
 	cd $dir
-	cd ..
 	fprime-util build --ut
-	cd $base_directory
 done
 
 #iterate over directories to check their tests
 for dir in "${dirs[@]}"
 do
-	echo -e "\n\nDetected component with unit test : $dir"
+	echo -e "\n\n\Checking component with unit test : $dir"
 	cd $dir
-	cd ..
 	fprime-util check
-	cd $base_directory
 done
+
 
 echo -e "\n\nTotal : Checked $processed_tests components for unit tests"
