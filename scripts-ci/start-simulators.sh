95368d8eebdb36a0f8632066da198e177d3f3b36
Lewis Jaggi <lewisjaggi@gmail.com>
2021-01-19 20:40:10 +0100

    try show venv

--- scripts-ci/start-simulators.sh
+++ scripts-ci/start-simulators.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-#imports necessary for the simulators
-python -m pip install python-statemachine
-
-#simulators start
-python simulators/ADCS/TcpMain.py &
-
-#start zmq
-sudo chmod 777 simulators/EPS/zmqproxy
-sudo ./simulators/EPS/zmqproxy &
 
 #launch EPS simulator
+pwd
 echo $LD_LIBRARY_PATH
-python simulators/EPS/CspMain.py &
