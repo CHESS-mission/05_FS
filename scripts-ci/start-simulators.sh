11725d046e825e6f91245962f7c314c6f84cc961
Lewis Jaggi <lewisjaggi@gmail.com>
2021-01-19 20:45:35 +0100

    jpp

--- scripts-ci/start-simulators.sh
+++ scripts-ci/start-simulators.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+#imports necessary for the simulators
+python -m pip install python-statemachine
+
+#simulators start
+python simulators/ADCS/TcpMain.py &
+
+#start zmq
+sudo chmod 777 simulators/EPS/zmqproxy
+sudo ./simulators/EPS/zmqproxy &
 
 #launch EPS simulator
-pwd
-echo $LD_LIBRARY_PATH
+sudo chmod 777 simulators/EPS/packages/csp/lib/libcsp.so simulators/EPS/packages/csp/lib/libcsp_py3.so
+python simulators/EPS/CspMain.py &
